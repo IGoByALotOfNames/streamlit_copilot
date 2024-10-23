573939dd19087b952efeb07ede8122767f33863c
Ivan He <108038846+ivanhe123@users.noreply.github.com>
2024-10-23 20:07:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -81,6 +81,8 @@ if st.session_state.username_flag:
     result_path=f"{st.session_state.user}_res"  #The folder path that you want to save the results
     st.title(f"你好！{st.session_state.user}")
     times = [0,2,4,7,15]
+    result_path=f"{st.session_state.user}_res"  #The folder path that you want to save the results
+    st.markdown(f"<p style='text-align: right;'>第{(st.session_state.progress//10+1)*10}天</p>", unsafe_allow_html = True)
     st.progress(st.session_state.progress*10)
 
     review_list = find_files(st.session_state.user, times)
