6e2e271a0a221447f182b16e2b67a95f44939534
Ivan He <108038846+ivanhe123@users.noreply.github.com>
2024-10-23 14:17:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -31,16 +31,17 @@ def login():
     if st.session_state.username_flag:
         st.rerun()
 # Function to find all files N days after the date the file was stored
-def find_files(directory, days_list):
+def find_files(usr_name, days_list):
     # List to store the matching files
     matching_files = []
-    if os.path.isdir(directory):
-        # Iterate through files in the directory
-        for filename in os.listdir(directory):
-            # Extract date from filename
-            file_date_str = filename
-    
-            file_date = datetime.strptime(file_date_str.split(".")[0], '%Y/%m/%d')
+
+    for filename in os.listdir():
+        # Extract date from filename
+        file_date_str = filename
+
+        if file_date_str.split("-")[0] == usr_name:
+
+            file_date = datetime.strptime(file_date_str.split("-")[1].replace(".png",""), '%Y/%m/%d')
     
             # Check if file matches any of the days in the list
             for days in days_list:
@@ -51,68 +52,7 @@ def find_files(directory, days_list):
 
     return matching_files
 
-def clearUp(dataset_path, model_path, result_path):
-    
-    input_size = [1024, 1024]
-    net = ISNetDIS()
-    net.load_state_dict(torch.load(model_path, map_location="cpu"))
-    net.eval()
-    im_list = [datas[0] for datas in dataset_path]
-    with torch.no_grad():
-        for i, im_path in enumerate(im_list):
-            im = cv2.imread(im_path)
-            if len(im.shape) < 3:
-                im = np.stack([im] * 3, axis=-1)  # Convert grayscale to RGB
-            im_shp = im.shape[0:2]
-            im_tensor = torch.tensor(im, dtype=torch.float32).permute(2, 0, 1)
-            im_tensor = F.upsample(torch.unsqueeze(im_tensor, 0), input_size, mode="bilinear").type(torch.uint8)
-            image = torch.divide(im_tensor, 255.0)
-            image = normalize(image, [0.5, 0.5, 0.5], [1.0, 1.0, 1.0])
-            if torch.cuda.is_available():
-                image = image.cuda()
-            result = net(image)
-            result = torch.squeeze(F.upsample(result[0][0], im_shp, mode='bilinear'), 0)
-            ma = torch.max(result)
-            mi = torch.min(result)
-            result = (result - mi) / (ma - mi)
-            result = result.unsqueeze(0) if result.dim() == 2 else result  # Ensure result has 3 channels
-            result = result.repeat(3, 1, 1) if result.shape[0] == 1 else result
-            result = 1 - result  # Invert the mask here
-
-            if torch.cuda.is_available():
-                result = result.cuda()  # Move result to GPU if available
-
-            im_name = im_path.split('\\')[-1].split('.')[0]
-
-            # Resize the image to match result dimensions
-            image_resized = F.upsample(image, size=result.shape[1:], mode='bilinear')
-
-            # Ensure both tensors are 3D
-            image_resized = image_resized.squeeze(0) if image_resized.dim() == 4 else image_resized
-            result = result.squeeze(0) if result.dim() == 4 else result
-
-            # Apply threshold to result to ensure only pure black or white pixels
-            threshold = 0.80  # Adjust as needed
-            result[result < threshold] = 0
-            result[result >= threshold] = 1
-
-            distance = np.sqrt(np.sum((im - [255, 255, 255]) ** 2, axis=-1))
-
-            # Create a mask where the distance is less than the threshold
-            mask = distance < 200
-
-            # Convert mask to uint8
-            mask = mask.astype(np.uint8) * 255
-
-            mask = np.stack([mask] * 3, axis=-1)
-
-
-            result = (result.permute(1, 2, 0) * 255).cpu().numpy().astype(np.uint8)
-            # result=result.cpu().numpy().astype(np.uint8)
-            # io.imsave(result_path + im_name + "_foreground.png", foreground)
-            wite = np.ones_like(im) * 255
-            cropped = np.where(result == 0, wite, mask)
-            cv2.imwrite(result_path + im_name + "_background.png", cropped)
+def clearUp(dataset_path):
     for datas in dataset_path:
         if datas[1] == 15:
             os.remove(datas[0])
@@ -137,7 +77,7 @@ if st.session_state.username_flag:
     result_path=f"{st.session_state.user}_res"  #The folder path that you want to save the results
     st.title(f"你好！{st.session_state.user}")
     times = [0,2,4,7,15]
-    review_list = find_files(dataset_path, times)
+    review_list = find_files(st.session_state.user, times)
     os.system("ls")
     if len(review_list) == 0:
         delete_page = st.Page("wrong_questions.py", title="错题分析", icon=":material/notification_important:")
@@ -146,7 +86,7 @@ if st.session_state.username_flag:
         calendar = st.Page("calender.py", title="打卡记录", icon = ":material/dashboard:")
         pg = st.navigation({"面板":[calendar],"打卡":[delete_page],"账号":[create_page]})
     else:
-        #done = clearUp(review_list, model_path, result_path)
+        done = clearUp(review_list)
         if done:
             pg=st.navigation([st.Page("review.py", title="复习")])
 else:
