619609403962f3c4dd0ede59ee178b9a5043878f
Ivan He <108038846+ivanhe123@users.noreply.github.com>
2024-10-23 14:50:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,16 +38,18 @@ def find_files(usr_name, days_list):
     for filename in os.listdir():
         # Extract date from filename
         file_date_str = filename
-        st.write(file_date_str.split("-")[0])
-        if file_date_str.split("-")[0] == usr_name:
 
-            file_date = datetime.strptime(file_date_str.split("-")[1].replace(".png",""), '%Y_%m_%d')
-            # Check if file matches any of the days in the list
-            for days in days_list:
-                target_date = file_date + timedelta(days=days)
-                target_date_str = target_date.strftime('%Y_%m_%d')
-                if os.path.exists(target_date_str+".png"):
-                    matching_files.append([target_date_str+".png",days])
+        if file_data_str.split(".")[1] == ".png":
+            st.write(file_date_str.split("-")[0])
+            if file_date_str.split("-")[0] == usr_name:
+    
+                file_date = datetime.strptime(file_date_str.split("-")[1].replace(".png",""), '%Y_%m_%d')
+                # Check if file matches any of the days in the list
+                for days in days_list:
+                    target_date = file_date + timedelta(days=days)
+                    target_date_str = target_date.strftime('%Y_%m_%d')
+                    if os.path.exists(target_date_str+".png"):
+                        matching_files.append([target_date_str+".png",days])
 
     return matching_files
 
