08a6601ad7031dff883b52f94f4cee0494cb5943
Ivan He <108038846+ivanhe123@users.noreply.github.com>
2024-10-23 20:10:48 +0800

    put down 0

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ if st.session_state.username_flag:
     dataset_path=f"{st.session_state.user}_wr"  #Your dataset path
     model_path="isnet.pth"  # the model path
     result_path=f"{st.session_state.user}_res"  #The folder path that you want to save the results
-    st.markdown(f"<p style='text-align: left;'>第{(st.session_state.progress//10)*10}天</p>", unsafe_allow_html = True)
+    st.markdown(f"<p style='text-align: left; position: absolute; top:15px;'>第{(st.session_state.progress//10)*10}天</p>", unsafe_allow_html = True)
     st.markdown(f"<p style='text-align: right;'>第{(st.session_state.progress//10+1)*10}天</p>", unsafe_allow_html = True)
     st.progress(st.session_state.progress*10)
 
