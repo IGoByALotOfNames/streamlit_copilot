096ea71c1c2bb88cf25b5e5187afb12324a34161
Ivan He <108038846+ivanhe123@users.noreply.github.com>
2024-10-23 19:32:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -81,6 +81,7 @@ if st.session_state.username_flag:
     result_path=f"{st.session_state.user}_res"  #The folder path that you want to save the results
     st.title(f"你好！{st.session_state.user}")
     times = [0,2,4,7,15]
+    st.progress(st.session_state.progress*10)
 
     review_list = find_files(st.session_state.user, times)
     
